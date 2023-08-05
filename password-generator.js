93a51de54be2aad14d235e40befed2ba472f7452
Dolapo Lawal <dolapolawal@Dolapos-MacBook-Pro.local>
2023-08-05 04:00:26 +0100

    nodepoint

---
+++ password-generator.js
@@ -0,0 +1,5 @@
+const passwordGenerator = require("generate-password");
+
+// Generate and console.log a random password
+const password = passwordGenerator.generate();
+console.log("Generated Password:", password);
